23bb9def6e1305b83d2bb889e5ca64b08244ac8e
obim04 <146782288+obim04@users.noreply.github.com>
2024-10-11 16:25:39 +0200

    Update Arm.py

--- Arm.py
+++ Arm.py
@@ -1,26 +1,28 @@
 from transitions import Machine
 from serial.tools import list_ports
-from pydobot import Dobot
+import DoBotArm as dbt
+import threading
+import time
 
 class Arm():
     
     def __init__(self):
-        self.position = None
-        self.port = list_ports.comports()[0].device
-        self.device = Dobot(port=self.port)
-        self.home = self.device.get_pose()
-        print(self.port)
+        self.position = None  
+        self.device = dbt.DoBotArm("COM6", 225, 0, -43, home=False)  
 
-        
+    def home(self):
+        """Retrieve the current position of the arm"""
+        self.position = self.device.getPosition()
+        return self.position
 
     def get_position(self, pos):
+        """Set the arm's position"""
         self.position = pos
     
     def go_to_position(self):
-        pos = self.position
-        self.device.move_to(pos[0], pos[1], 50, None, wait=True)
-        self.device.move_to(pos[0], pos[1], -50, None, wait=True)
-    
-    def go_home(self):
-        self.device.move_to(self.home.x, self.home.y, self.home.z, self.home.r, wait=True)
-
+        """Move the arm to the current position"""
+        if self.position is not None:
+            pos = self.position
+            self.device.moveArmXYZ(x=pos[0], y=pos[1], z=pos[2])
+        else:
+            print("Error: Position not set")
